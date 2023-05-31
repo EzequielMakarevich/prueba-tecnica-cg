765752503e21182f3a58aea63237fc99fc8b4eb7
EzequielMakarevich <ezequielmakarevich@gmail.com>
2023-05-31 19:05:52 -0300

    added products list

--- src/app/infrastructure/services/products.service.ts
+++ src/app/infrastructure/services/products.service.ts
@@ -1,15 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IProducts } from '../entities/products.interface';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  url = 'https://static.compragamer.com/test/'
+  url = 'https://static.compragamer.com/test/';
   constructor(private http: HttpClient) { }
   getProductsData() {
-    return this.http.get<IProducts[]>(this.url + 'productos.json')
+    return this.http.get<IProducts[]>(this.url + 'productos.json');
   }
 }
